08bb5f453ebe7761c4d317b463739fbf078e665a
NHD <nguyenhoangdao1609@.gmail.com>
2024-04-16 10:29:37 +0700

    Simple board routing

--- src/server.js
+++ src/server.js
@@ -1,16 +1,10 @@
 /* eslint-disable no-console */
 
-
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 //import exitHook from 'async-exit-hook'
 import express from 'express'
 import { env } from '~/config/environment'
 import { CONNECT_DB } from './config/mongodb'
+import { APIs_V1 } from '~/src/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
@@ -18,10 +12,7 @@ const START_SERVER = () => {
   const hostname = 'localhost'
   const port = 8017
 
-  app.get('/', async (req, res) => {
-
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(port, hostname, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
